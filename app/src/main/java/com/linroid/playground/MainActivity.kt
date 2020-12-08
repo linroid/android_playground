38b8235b5ee7e3e87483b547538335fbb4a5c907
Lin Zhang <linroid@gmail.com>
2020-12-08 16:48:58 +0800

    Add benchmark module

--- app/src/main/java/com/linroid/playground/MainActivity.kt
+++ app/src/main/java/com/linroid/playground/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import com.linroid.playground.binder.AidlActivity
 import kotlinx.android.synthetic.main.main_activity.*
 
+/**
+ * Created by linroid on 11/18/20.
+ */
 class MainActivity : AppCompatActivity(R.layout.main_activity) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
